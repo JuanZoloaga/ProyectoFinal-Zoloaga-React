0684d924eeb5fb0de5ed8dc939af648c7698831f
Juan Zoloaga <juan.zoloaga5@hotmail.com>
2024-09-19 11:01:52 -0300

    correccionesPrimeraPreEntrega-React

--- src/assets/componentes/NavBarComponente.jsx
+++ src/assets/componentes/NavBarComponente.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/prop-types */
 import '../componentes/NavBarComponente.css';
+import LogoComponente from './LogoComponente';
+import CartWidget from './CartWidget';
+import SloganComponente from './SloganComponente';
 
-export default function NavBarComponente ( {texto} ){
+export default function NavBarComponente ( ){
     return(
         <>
-        <li className='nav'><a href='../index.html'>{texto}</a></li>
+        <nav className='navbar'>
+            <div className='logotipo'>
+                <LogoComponente/>
+            </div>
+            <div className='navbarTitulos'>
+                <button>Producto</button>
+                <button>Sucursal</button>
+                <button>Nosotrosos</button>
+            </div>
+                <CartWidget/>
+        </nav>
+        <SloganComponente texto='Comer saludable es un arte que se aprende.'></SloganComponente>
         </>
     );
 
--- src/assets/componentes/SloganComponente.jsx
+++ src/assets/componentes/SloganComponente.jsx
@@ -4,7 +4,9 @@ import './SloganComponente.css'
 export default function SloganComponente({texto}){
     return(
         <>
+        <div className='textoslogan'>
             <p className='slogan'>{texto}</p>
+        </div>
         </>
     );
 
